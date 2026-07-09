80681ccd8f1dca39bdab5765ceb2a1224c7880da
yeonoh2402596 <jooyeonoh3526777@gmail.com>
2026-07-09 17:41:50 +0900

    쿼리 수정

--- data/webjjang_sample.sql
+++ data/webjjang_sample.sql
@@ -10,12 +10,14 @@ VALUES('test', PASSWORD(id), '홍길동', '남자', '1400-01-01', '010-1111-2222
 INSERT INTO member_roles VALUES('test', 'ROLE_USER');
 
 -- 회원 데이터
-INSERT INTO	member(id, pq, NAME, gender, birth, tel, post_no, address, email)
+INSERT INTO	member(id, pw, NAME, gender, birth, tel, post_no, address, email)
 VALUES('admin', PASSWORD(id), '관리자', '여자', '2000-04-11', '010-3333-4444', '04780',
 	'서울시 성수동 1가', 'admin@gmail.com');
 
 -- 회원권한
 INSERT INTO member_roles VALUES('admin', 'ROLE_USER');
 INSERT INTO member_roles VALUES('admin', 'ROLE_ADMIN');
+
+COMMIT;
 
 COMMIT;
